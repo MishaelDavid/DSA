b362b2305cc4e208d17dc6fafdd27a398ba0e4ea
MishaelDavid <mishaeldavid007@gmail.com>
2024-08-10 12:43:14 +0530

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         }
         System.out.println("you entered :");
         for(int i=0;i<n;i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]);//hola
         }
 
     }
